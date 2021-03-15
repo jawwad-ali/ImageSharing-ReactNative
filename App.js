395360bee4607acb3190a593eb0e2e961fe26944
jawwad-ali <alijawwad001@gmail.com>
2021-03-15 21:31:58 +0500

    image sharing app using react-native

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as ImagePicker from "expo-image-picker"
+import * as Sharing from "expo-sharing"
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  // func to choose an image from gallery
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.getMediaLibraryPermissionsAsync()
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera is required')
+      // return
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync()
+    console.log(pickerResult)
+
+    if (pickerResult.cancelled === true) {
+      return
+    }
+    setSelectedImage({ localUri: pickerResult.uri })
+  }
+
+  // image sharing func
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert('sharing not available on your platform ')
+      // return
+    }
+    await Sharing.shareAsync(selectedImage.localUri)
+  }
+
+
+  // getting the selected images from gallery
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity onPress={openShareDialogAsync}>
+          <Text style={styles.shareButton}>Share the photo</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
+      <Text style={styles.instruction}>To share an image with friend click the button below!</Text>
+
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Pick a Photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +69,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20
+  },
+  instruction: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain" 
+  },
+  shareButton:{
+    fontSize: 20,
+    color: '#fff',
+    backgroundColor: 'black',
+    padding: 20,
+    marginTop:20,
+    borderRadius: 5,
+  }
+
 });
